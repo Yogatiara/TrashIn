f64c119f949742a5cddefd26272b804b90e73aaa
Djibran1103 <11211070@student.itk.ac.id>
2023-11-17 21:29:11 +0800

    Dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import Dashboard from "./pages/Dashboard";
 
 const App = () => {
   return (
     <>
-      <LandingPage />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>    
     </>
+    
   );
 };
 
---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import Sidebar from "../components/Sidebar";
+
+const Dashboard = () => {
+    return ( 
+        <div className="bg-green-500 h-8">
+            Test
+            <Sidebar />
+        </div>  
+     );
+}
+ 
+export default Dashboard;
