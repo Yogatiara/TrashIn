9295db827b8698abf4dd3561893a3cd6f2790091
Djibran1103 <11211070@student.itk.ac.id>
2023-11-18 20:29:51 +0800

    login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Dashboard from "./pages/Dashboard";
 import ListUser from "./components/dashboard/ListUser";
 import ListEvent from "./components/Dashboard/ListEvent";
 import ListTps from "./components/Dashboard/ListTps";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
           <Route path="/dashboard/list-user" element={<ListUser />} />
           <Route path="/dashboard/list-tps" element={<ListTps />} />
           <Route path="/dashboard/list-event" element={<ListEvent />} />
+          <Route path="/login-admin" element={<Login />} />
         </Routes>
       </BrowserRouter>    
     </>
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,35 @@
+import LoginForm from "../components/Login/LoginForm";
+
+const Login = () => {
+  return (
+    <aside style={{ display: "flex", flexDirection: "row", height: "100vh" }}>
+      <div className="sidebar">
+        <div className="h-screen flex flex-col justify-center bg-[#2FC8B0]">
+          <div
+            href="/"
+            className="flex items-center justify-center ps-2.5 gap-1.5 mb-9 mt-6 ml-8"
+          >
+            <img
+              className="h-16 sm:h-10 scale-150"
+              src="./images/Logo-TrashIn.png"
+              alt="Logo"
+            />
+            <span className="whitespace-nowrap font-black scale-0.5 mr-10 mt-2">
+              <img src="./images/rashIn.png" alt="" />
+            </span>
+          </div>
+          <LoginForm />
+        </div>
+      </div>
+      <div className="content" style={{ flex: 6 }}>
+        <img
+          className="object-cover w-full h-full brightness-50"
+          src="./images/tpa.png"
+          alt="tpa image"
+        />
+      </div>
+    </aside>
+  );
+};
+
+export default Login;
